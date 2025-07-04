cbad49d47a1c730a9541852abbc31eae05b8f75d
QTChosusTV <qtchosustv1109@gmail.com>
2025-07-04 14:37:09 +0700

    Fix #8 BQTOJ

--- src/app/post-register/page.tsx
+++ src/app/post-register/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { supabase } from '@/utils/supabaseClient'
 
 export default function PostRegisterPage() {
