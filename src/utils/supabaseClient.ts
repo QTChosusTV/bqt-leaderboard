0cf727db798a17838a07b23e4a4497ac0bd1d3a3
QTChosusTV <qtchosustv1109@gmail.com>
2025-07-04 11:44:39 +0700

    Fix #5 for BQTOJ

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,9 @@
 'use client'
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL // eslint-disable-line @typescript-eslint/no-unused-vars
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // eslint-disable-line @typescript-eslint/no-unused-vars
+
 export const supabase = createPagesBrowserClient()
 
 
